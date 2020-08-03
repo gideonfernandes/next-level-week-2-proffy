3a58d526f587ddd012a5e13214331530b4a6d611
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-08-03 18:21:18 -0300

    Add setup with create react-app & assets

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <button>Omnistack!</button>
+    </div>
+  );
+};
+
+export default App;
