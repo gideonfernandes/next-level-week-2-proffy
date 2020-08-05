156460be3857057281c5aac21a2c151cc696e017
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-08-04 22:09:51 -0300

    Add create class route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World from API!' });
-});
+app.use(routes);
 
 const PORT = process.env.PORT || 8888;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
